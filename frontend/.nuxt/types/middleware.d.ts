277467ead87538e395d74a5fb6601889798b0c6e
thehongbaokhanh <thehongbaokhanh2005@gmail.com>
2026-09-04 15:13:10 +0700

    chore: stop tracking Nuxt generated files

--- frontend/.nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = "admin" | "auth" | "company" | "enterprise-approved" | "guest" | "student"
-declare module 'nuxt/app' {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
